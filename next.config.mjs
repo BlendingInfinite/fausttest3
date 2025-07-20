2df8176c8430ee47eeb8619e9e1c4c7ee4c9a54b
Moritz Nakatenus <moritz.nakatenus@protonmail.com>
2025-07-20 11:31:01 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
+      },
+    },
+  },
 };
 
 export default nextConfig;
